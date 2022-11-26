d855ad3b394eb112aa3d9984b82283dbb268d218
Elena Korchuganova <korchel21@gmail.com>
2022-11-26 15:13:08 +0300

    step 1

---
+++ bin/gendiff.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+
+const program = new Command();
+program.version('output the version number').description('Compares two configuration files and shows a difference.');
+program.parse();
