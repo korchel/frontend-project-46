16ace4d06e518220d1a3a5dd2896c2445ec560bb
Elena Korchuganova <korchel21@gmail.com>
2022-12-18 15:45:34 +0300

    step 6/1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,8 @@
 import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import * as fs from 'fs';
 import genDiff from '../src/index.js';
-
-const expectedResult = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,15 +13,16 @@ const filePath1 = getFixturePath('file1.json');
 const filePath2 = getFixturePath('file2.json');
 const filePath3 = getFixturePath('file3.yml');
 const filePath4 = getFixturePath('file4.yml');
+const expectedResult = fs.readFileSync(getFixturePath('expectedResult.txt'), 'utf8');
 
-test('genDiff in flat json files', () => {
+test('genDiff in json files', () => {
   expect(genDiff(filePath1, filePath2)).toEqual(expectedResult);
 });
 
-test('genDiff in flat yaml files', () => {
+test('genDiff in yaml files', () => {
   expect(genDiff(filePath3, filePath4)).toEqual(expectedResult);
 });
 
-test('genDiff in flat yaml and json files', () => {
+test('genDiff in yaml and json files', () => {
   expect(genDiff(filePath1, filePath4)).toEqual(expectedResult);
 });
