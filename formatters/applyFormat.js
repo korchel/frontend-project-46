834fa633dc9079acb507468f5597d8db6cba498f
Elena Korchuganova <korchel21@gmail.com>
2023-01-05 22:14:54 +0300

    step 6/3

--- formatters/applyFormat.js
+++ formatters/applyFormat.js
@@ -1,18 +1,6 @@
 import _ from 'lodash';
 import applyStylishFormat from './stylish.js';
 
-const getKeyStatus = (key, object1, object2) => {
-  if (_.has(object1, key) && _.has(object2, key)) {
-    if (_.isEqual(object1[key], object2[key])) {
-      return 'unchanged';
-    }
-    return 'changed';
-  } if (!_.has(object1, key)) {
-    return 'added';
-  }
-  return 'deleted';
-};
-
 const stringifyObject = (object) => {
   const iter = (data, level) => {
     const currentIndent = '  '.repeat(level);
@@ -38,4 +26,4 @@ const applyFormat = (object1, object2, format) => {
   }
 };
 
-export { getKeyStatus, applyFormat };
+export default applyFormat;
--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,27 +1,38 @@
 import _ from 'lodash';
-import { getKeyStatus } from './applyFormat.js';
+
+const getKeyStatus = (key, object1, object2) => {
+  if (_.has(object1, key) && _.has(object2, key)) {
+    if (_.isEqual(object1[key], object2[key])) {
+      return 'unchanged';
+    }
+    return 'changed';
+  } if (!_.has(object1, key)) {
+    return 'added';
+  }
+  return 'deleted';
+};
 
 const applyStylishFormat = (object1, object2) => {
-  const keys = _.union(_.keys(object1), _.keys(object2));
+  const keys = _.sortBy(_.union(_.keys(object1), _.keys(object2)), (key) => key);
   const diff = keys.reduce((acc, key) => {
     const checkKey = getKeyStatus(key, object1, object2);
     switch (checkKey) {
       case 'unchanged':
-        acc[`  ${key}`] = object1[key];
+        acc[`  ${key}`] = _.isObject(object1[key]) ? applyStylishFormat(object1[key], object1[key]) : object1[key];
         break;
       case 'changed':
-        if (_.isObject(object1[key])) {
+        if ((_.isObject(object1[key]) && _.isObject(object2[key]))) {
           acc[`  ${key}`] = applyStylishFormat(object1[key], object2[key]);
         } else {
-          acc[`- ${key}`] = object1[key];
-          acc[`+ ${key}`] = object2[key];
+          acc[`- ${key}`] = _.isObject(object1[key]) ? applyStylishFormat(object1[key], object1[key]) : object1[key];
+          acc[`+ ${key}`] = _.isObject(object2[key]) ? applyStylishFormat(object2[key], object2[key]) : object2[key];
         }
         break;
       case 'added':
-        acc[`+ ${key}`] = object2[key];
+        acc[`+ ${key}`] = _.isObject(object2[key]) ? applyStylishFormat(object2[key], object2[key]) : object2[key];
         break;
       case 'deleted':
-        acc[`- ${key}`] = object1[key];
+        acc[`- ${key}`] = _.isObject(object1[key]) ? applyStylishFormat(object1[key], object1[key]) : object1[key];
         break;
       default:
         return 'no such key';
