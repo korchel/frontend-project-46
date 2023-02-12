63cc2c4edba0d84a091b7491c5afcb79e37d4b32
Elena Korchuganova <korchel21@gmail.com>
2023-02-12 20:58:49 +0300

    check 1/2

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,40 +9,26 @@ const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const filePath1 = getFixturePath('file1.json');
-const filePath2 = getFixturePath('file2.json');
-const filePath3 = getFixturePath('file3.yml');
-const filePath4 = getFixturePath('file4.yml');
 const expectedResultForStylish = fs.readFileSync(getFixturePath('expectedResultForStylish.txt'), 'utf8');
 const expectedResultForPlain = fs.readFileSync(getFixturePath('expectedResultForPlain.txt'), 'utf8');
 
-const stylishTestData = [
-  { file1: filePath1, file2: filePath2, expected: expectedResultForStylish },
-  { file1: filePath3, file2: filePath4, expected: expectedResultForStylish },
-  { file1: filePath1, file2: filePath4, expected: expectedResultForStylish },
-];
+const testFormats = ['json', 'yml'];
 
-const plainTestData = [
-  { file1: filePath1, file2: filePath2, expected: expectedResultForPlain },
-  { file1: filePath3, file2: filePath4, expected: expectedResultForPlain },
-  { file1: filePath1, file2: filePath4, expected: expectedResultForPlain },
-];
-
-const jsonTestData = [
-  { file1: filePath1, file2: filePath2 },
-  { file1: filePath3, file2: filePath4 },
-  { file1: filePath1, file2: filePath4 },
-];
-
-test.each(stylishTestData)('genDiff in $file1 and $file2 files in stylish format', ({ file1, file2, expected }) => {
-  expect(genDiff(file1, file2)).toEqual(expected);
+test.each(testFormats)('genDiff in $format files in stylish format', (format) => {
+  const filepath1 = getFixturePath(`file1.${format}`);
+  const filepath2 = getFixturePath(`file2.${format}`);
+  expect(genDiff(filepath1, filepath2)).toEqual(expectedResultForStylish);
 });
 
-test.each(plainTestData)('genDiff in $file1 and $file2 files in plain format', ({ file1, file2, expected }) => {
-  expect(genDiff(file1, file2, 'plain')).toEqual(expected);
+test.each(testFormats)('genDiff in $format files in plain format', (format) => {
+  const filepath1 = getFixturePath(`file1.${format}`);
+  const filepath2 = getFixturePath(`file2.${format}`);
+  expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedResultForPlain);
 });
 
-test.each(jsonTestData)('genDiff in $file1 and $file2 files in json format', ({ file1, file2 }) => {
-  const diff = genDiff(file1, file2, 'json');
+test.each(testFormats)('genDiff in $format files in json format', (format) => {
+  const filepath1 = getFixturePath(`file1.${format}`);
+  const filepath2 = getFixturePath(`file2.${format}`);
+  const diff = genDiff(filepath1, filepath2, 'json');
   expect(() => JSON.parse(diff)).not.toThrow();
 });
